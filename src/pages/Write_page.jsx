2d05ef49704d2b38c2538640b670307dfb8cf9d6
soberanalysts <nss262@naver.com>
2025-01-03 22:28:49 +0900

    [Feature]게시글 작성(DB없이 임시 백엔드서버로 )

--- src/pages/Write_page.jsx
+++ src/pages/Write_page.jsx
@@ -1,28 +1,21 @@
 import {useState} from "react";
+import PostBoard from "../components/board/postboard";
 
 const WritePage = () => {
-    let [inputCount, setInputCount] = useState(0);
-    let [textCount, setTextareaCount] = useState(0);
+    // let [inputCount, setInputCount] = useState(0);
+    // let [textCount, setTextareaCount] = useState(0);
 
-    const textCounter = (e) => {    //textarea에 입력한 글자 세는 함수
-        setTextareaCount(e.target.value.replace(/[\0-\x7f]|([0-\u07ff]|(.))/g, "$&$1$2").length);
-        //한글일 경우 3Byte, 그외 1Byte로 계산
-    }
+    // const textCounter = (e) => {    //textarea에 입력한 글자 세는 함수
+    //     setTextareaCount(e.target.value.replace(/[\0-\x7f]|([0-\u07ff]|(.))/g, "$&$1$2").length);
+    //     //한글일 경우 3Byte, 그외 1Byte로 계산
+    // }
 
-    const inputCounter = (e) => {   //input에 입력한 글자 세는 함수
-        setInputCount(e.target.value.replace(/[\0-\x7f]|([0-\u07ff]|(.))/g, "$&$1$2").length);
-    }
-    // const res = await fetch('/api/board/createPost', {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({ title, content }),
-    //   });
-
+    // const inputCounter = (e) => {   //input에 입력한 글자 세는 함수
+    //     setInputCount(e.target.value.replace(/[\0-\x7f]|([0-\u07ff]|(.))/g, "$&$1$2").length);
+    // }
     return(
         <div>
-            <input
+            {/* <input
                 type="text"
                 placeholder="Enter hour title here..."
                 className="form-control mb-3 " // 가로 길이를 늘림
@@ -37,7 +30,8 @@ const WritePage = () => {
             />
             <h6>{textCount}words</h6>
             <button className="btn btn-outline-secondary">Cancel</button>
-            <button className="btn btn-primary">Publish</button>
+            <button className="btn btn-primary">Publish</button> */}
+            <PostBoard/>
         </div>
     );
 }
