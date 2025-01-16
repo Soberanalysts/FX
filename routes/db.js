cf8cb79dc1f76593823429cfa4808df260db7b4d
Joon <wushu80@hanmail.net>
2025-01-16 22:40:13 +0900

    2025.01.16 DB 연결 오류 수정 등

--- routes/db.js
+++ routes/db.js
@@ -2,14 +2,17 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '.env.development' });
 import mariadb from 'mariadb';
 
-// MariaDB Connection Pool 생성 (커넥션 5개)
+// MariaDB Connection Pool 생성
 const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  connectionLimit: 10,
+  connectionLimit: 10, // 커넥션 갯수 상한선
+  idleTimeout: 30, // 커넥션이 반환된 후 idle timeout 시간. wait_timeout 시스템 변수보다 작아야 한다.
+  // leakDetectionTimeout: 12, // 풀에서 빌려온 커넥션에 대해서 몇 초뒤부터 로깅할지 설정
+  trace: true, // 개발시 initial stack trace 용도
 });
 
 export default Object.freeze(pool);
--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import debug from 'debug';
 import dbPool from './db.js';
 // import { getUser } from './userRouter.js';
+
+const debugLog = new debug('log');
+const debugError = new debug('error');
+const debugDb = new debug('db');
+
 
 const router = express.Router();
 let conn; // DB Connection Pool로부터 얻어온 커넥션을 저장할 변수
@@ -21,10 +27,10 @@ async function getPost(postId) {
     if (error.code === 'ER_CONNECTION_TIMEOUT') {
       res.status(500).json({ message: 'Connection Timeout' });
     }
-    console.log(error);
+    debugDb(error);
   } finally {
     if (conn) {
-      conn.release();
+      await conn.release();
     }
   }
 }
@@ -49,10 +55,10 @@ router.post('/', async (req, res) => {
     if (error.code === 'ER_CONNECTION_TIMEOUT') {
       res.status(500).json({ message: 'Connection Timeout' });
     }
-    console.log(error);
+    debugDb(error);
   } finally {
     if (conn) {
-      conn.release();
+      await conn.release();
     }
   }
 });
@@ -63,6 +69,7 @@ router.get('/:id', async (req, res) => {
   try {
     const post = await getPost(postId);
     if (post?.post_id) {
+      debugDb('게시글 조회 완료');
       res.status(200).json({
         message: '게시글 조회가 완료되었습니다.',
         post: post
@@ -74,7 +81,7 @@ router.get('/:id', async (req, res) => {
     if (error.code === 'ER_CONNECTION_TIMEOUT') {
       res.status(500).json({ message: 'Connection Timeout' });
     }
-    console.log(error);
+    debugDb(error);
   } finally {
     if (conn) {
       await conn.release();
@@ -108,10 +115,10 @@ router.put('/:id', async (req, res) => {
     if (error.code === 'ER_CONNECTION_TIMEOUT') {
       res.status(500).json({ message: 'Connection Timeout' });
     }
-    console.log(error);
+    debugDb(error);
   } finally {
     if (conn) {
-      conn.release();
+      await conn.release();
     }
   }
 })
@@ -139,10 +146,10 @@ router.delete('/:id', async (req, res) => {
     if (error.code === 'ER_CONNECTION_TIMEOUT') {
       res.status(500).json({ message: 'Connection Timeout' });
     }
-    console.log(error);
+    debugDb(error);
   } finally {
     if (conn) {
-      conn.release();
+      await conn.release();
     }
   }
 });
@@ -172,10 +179,10 @@ router.get('/', async (req, res) => {
     if (error.code === 'ER_CONNECTION_TIMEOUT') {
       res.status(500).json({ message: 'Connection Timeout' });
     }
-    console.log(error);
+    debugDb(error);
   } finally {
     if (conn) {
-      conn.release();
+      await conn.release();
     }
   }
 });
